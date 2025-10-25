29549abbeddda779bcc9cb6971781ae9154b97b9
Kshitiz Goel <167351635+Kshitiz-Goel@users.noreply.github.com>
2025-10-25 09:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (13.49%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int total=0;
-        int weeks=n/7;
-        int days=n%7;
-        for(int i=0;i<weeks;i++){
-            total+=(28+(7*i));
+        int total = 0;
+        int weeks = n / 7;
+        int days = n % 7;
+        for (int i = 0; i < weeks; i++) {
+            total += (28 + (7 * i));
         }
         weeks++;
-        for(int i=0;i<days;i++){
-            total+=weeks;
+        for (int i = 0; i < days; i++) {
+            total += weeks;
             weeks++;
         }
         return total;
