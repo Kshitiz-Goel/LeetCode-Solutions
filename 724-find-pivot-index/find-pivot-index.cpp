c3779f2a94fd9e0c80383b4ae218b1c03ff3ee8d
Kshitiz Goel <167351635+Kshitiz-Goel@users.noreply.github.com>
2025-11-10 19:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 35.5 MB (95.78%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        int temp=0;
+        for(int i=0;i<nums.size();i++){
+            if(temp==sum-temp-nums[i]) return i;
+            temp+=nums[i];
+        }
+        return -1;
+    }
+};
